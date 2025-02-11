c405324147ceead735ec53ec1686aa7bec6cbdf5
cliff <dunderjeep@gmail.com>
2025-02-11 17:25:24 -0500

    updated custom component

--- src/sections/home/custom-component.jsx
+++ src/sections/home/custom-component.jsx
@@ -1,6 +1,5 @@
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { CustomComponent, CustomComponent2 } from './custom-component'
 
 export function HomeHeroCYSE1008() {
   return (
@@ -8,8 +7,6 @@ export function HomeHeroCYSE1008() {
     <div>Hello World</div>
     <Box>Hi i'm a button</Box>
     <Button>Hi i'm a button</Button>
-    <CustomComponent />
-    <CustomComponent2 />
   </>
   );
 }
